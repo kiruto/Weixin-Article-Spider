17aa1fa84281b0ebff1d6060ae39e87a61415f4e
yuriel <yuriel3183@gmail.com>
2016-12-07 15:54:10 +0800

    fixed chrome extension

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -2,14 +2,14 @@
  * Created by yuriel on 12/5/16.
  */
 
-var TARGET_URL = 'http://www.chunyuyisheng.com/cyadmin/news/healthnews/add/';
+var TARGET_URL = 'https://www.chunyuyisheng.com/cyadmin/news/healthnews/add/';
 function openTarget(cb) {
     chrome.tabs.create({ url: TARGET_URL }, function(tab) {
         listener = function(tabId, info) {
             if(tab.id == tabId && info.status == "complete") {
                 cb(tab);
+                chrome.tabs.onUpdated.removeListener(listener);
             }
-            chrome.tabs.onUpdated.removeListener(listener);
         };
         chrome.tabs.onUpdated.addListener(listener);
     });
@@ -18,11 +18,15 @@ function openTarget(cb) {
 chrome.browserAction.onClicked.addListener(function() {
     chrome.tabs.query({active:true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {action: "get_article"}, function(response) {
-            // alert(response.article);
-            if ('article' in response) {
+            if ('article' in response && 'title' in response && 'author' in response) {
                 openTarget(function (tab) {
-                    chrome.tabs.sendMessage(tab.id, {action: "fill", text: response.article}, function (response) {
-                        alert(response.status);
+                    chrome.tabs.sendMessage(tab.id, {
+                        action: "fill",
+                        text: response.article,
+                        title: response.title,
+                        author: response.author
+                    }, function (response) {
+
                     });
                 });
             }
--- chrome_extension/main.js
+++ chrome_extension/main.js
@@ -10,30 +10,56 @@ function detectUEditor() {
     return typeof id_content !== 'undefined'
 }
 
-function getHTML() {
+function getElementInIFrame() {
     return document.getElementById('article-viewer')
         .getElementsByTagName('iframe')[0]
-        .contentDocument
-        .getElementsByTagName('body')[0]
-        .innerHTML
+        .contentDocument;
+}
+
+function getHTML() {
+    return getElementInIFrame()
+        .getElementById('js_content')
+        .innerHTML;
+}
+
+function getTitle() {
+    return getElementInIFrame()
+        .getElementById('activity-name')
+        .textContent
+        .trim();
+}
+
+function getAuthor() {
+    return getElementInIFrame()
+        .getElementById('post-user')
+        .textContent
+        .trim();
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (!detectArticleViewer()) return;
     if (request.action == "get_article")
-        sendResponse({article: getHTML()});
+        sendResponse({
+            title: getTitle(),
+            article: getHTML(),
+            author: getAuthor()
+        });
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (!detectUEditor()) return;
     if (request.action == "fill") {
+        console.log(request);
         let function_text = request.text.replace(/\"/g, "\\\"")
             .replace(/\'/g, "\\\'")
             .replace(/\n/g, '\\n');
-        let injectedCode = 'UE.getEditor(\'id_content\').setContent(\'' + function_text + '\')';
+        let injectedCode = 'UE.getEditor(\'id_content\').setContent(\'' + function_text + '\');';
+        injectedCode += 'document.getElementById(\'id_title\').setAttribute(\'value\', \''+ request.title +'\');';
+        injectedCode += 'document.getElementById(\'id_source\').setAttribute(\'value\', \''+ request.author +'\');';
         let script = document.createElement('script');
         script.textContent = injectedCode;
         (document.head || document.documentElement).appendChild(script);
         script.parentNode.removeChild(script);
+        sendResponse({status: 'ok'})
     }
 });
